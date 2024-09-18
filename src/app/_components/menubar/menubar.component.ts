dafb8df7d35a70eb560370954ee51a4160f34126
oliviaSkyeHolley <o.holley.aus@gmail.com>
2024-09-18 23:24:25 +1000

    fixed up some theming, tried to get logo up, was not working i will try again tomorrow

--- src/app/_components/menubar/menubar.component.ts
+++ src/app/_components/menubar/menubar.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
-import {NgIf} from "@angular/common";
+import {NgIf, NgOptimizedImage} from "@angular/common";
 import {AuthService} from "../../_services/auth.service";
 
 @Component({
   selector: 'app-menubar',
   standalone: true,
-    imports: [RouterModule, NgIf],
+  imports: [RouterModule, NgIf, NgOptimizedImage],
   templateUrl: './menubar.component.html',
   styleUrl: './menubar.component.scss'
 })
