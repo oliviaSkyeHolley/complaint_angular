cb1f198197fc02133b9d5beddeeca7dd84a61b4d
oliviaSkyeHolley <o.holley.aus@gmail.com>
2024-09-20 19:55:05 +1000

    Logo is up and sized right. Still need to make a small version

--- src/app/_components/authenticate/authenticate.component.ts
+++ src/app/_components/authenticate/authenticate.component.ts
@@ -6,6 +6,7 @@ import {FormsModule} from "@angular/forms";
 import { MatFormField } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+//import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-authenticate',
